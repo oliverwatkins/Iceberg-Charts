c416c9663a4d61604200311ec828392f913b64ff
Oliver Watkins <oliver.f.watkins@gmail.com>
2015-08-25 16:31:58 +0200

    asdf

---
+++ src/main/java/com/bluewalrus/chartmanager/ChartFile.java
@@ -0,0 +1,15 @@
+package com.bluewalrus.chartmanager;
+
+import java.io.Serializable;
+
+public class ChartFile implements Serializable{
+	
+	public ChartFile(String name, String location) {
+		super();
+		this.name = name;
+		this.location = location;
+	}
+	String name;
+	String location;
+
+}
